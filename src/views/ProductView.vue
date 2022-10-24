384d8d00985b3cc3f80d170559e5b35e94ecdf7e
Roman Pashnev <crsprt@gmail.com>
2022-10-24 23:51:08 +0800

    Добавить тестовые страницы

---
+++ src/views/ProductView.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Товары</h1>
+</template>
---
+++ src/views/SummaryView.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>Сводка</h1>
+  <div class="login">
+    <button v-if="!isAuthenticated" @click="accountIn">Log in</button>
+    <button v-if="isAuthenticated" @click="accountOut">Log out</button>
+  </div>
+  <div class="user">
+    <pre v-if="isAuthenticated">
+      <code>{{ user }}</code>
+      <code>{{ idTokenClaims }}</code>
+    </pre>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { useAuth0 } from '@auth0/auth0-vue';
+
+const {
+  loginWithRedirect,
+  logout,
+  user,
+  isAuthenticated,
+  idTokenClaims,
+} = useAuth0();
+
+const accountIn = () => {
+  loginWithRedirect();
+};
+
+const accountOut = () => {
+  logout({ returnTo: window.location.origin });
+};
+</script>
