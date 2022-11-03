25d91bcd94f842bd46bfa310ee7268f2e6a451a9
Roman Pashnev <crsprt@gmail.com>
2022-11-03 23:55:47 +0800

    Мелкий синтаксический рефакторинг

--- src/utils/EnvironmentUtils.ts
+++ src/utils/EnvironmentUtils.ts
@@ -1,5 +1,5 @@
 export default class EnvironmentUtils {
-  public static isProduction = (): boolean => process.env.NODE_ENV === 'production';
+  public static isProduction = (): boolean => (process.env.NODE_ENV === 'production');
 
-  public static isDevelopment = (): boolean => !EnvironmentUtils.isProduction();
+  public static isDevelopment = (): boolean => (!EnvironmentUtils.isProduction());
 }
