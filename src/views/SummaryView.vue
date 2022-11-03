092d8c9f89befae46abe0ee745761be572f53154
Roman Pashnev <crsprt@gmail.com>
2022-11-03 23:58:34 +0800

    Добавить тестовый вызов эндпоинтов сервера ресурсов

--- src/views/SummaryView.vue
+++ src/views/SummaryView.vue
@@ -1,3 +1,36 @@
 <template>
   <h1>Сводка</h1>
+  <p>{{ publicData }}</p>
+  <p>{{ privateData }}</p>
 </template>
+
+<script lang="ts" setup>
+import { useAuth0 } from '@auth0/auth0-vue';
+import { onMounted, ref } from 'vue';
+import { set } from '@vueuse/core';
+
+const { getAccessTokenSilently } = useAuth0();
+
+const publicData = ref<string>('');
+const privateData = ref<string>('');
+
+const getPublic = async () => {
+  const response = await fetch('https://api.midni.ru/api/public');
+  const data = await response.json();
+  set(publicData, data);
+};
+
+const getPrivate = async () => {
+  const token = await getAccessTokenSilently();
+  const response = await fetch('https://api.midni.ru/api/private', {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+  const data = await response.json();
+  set(privateData, data);
+};
+
+onMounted(() => {
+  getPublic();
+  getPrivate();
+});
+</script>
