be87110721c8ae8b1095589041deb45b9dfae892
Roman Pashnev <crsprt@gmail.com>
2022-11-07 00:36:54 +0800

    Перенести параметры конфигурации провайдера авторизации в .env

--- src/plugins/authProvider.ts
+++ src/plugins/authProvider.ts
@@ -1,10 +1,10 @@
 import { createAuth0 } from '@auth0/auth0-vue';
 
 const authProvider = createAuth0({
-  domain: 'midni.eu.auth0.com',
-  client_id: 'GU8b3MmSrnm5vXplF4QAKl4lPt3KGOd6',
+  domain: process.env.VUE_APP_AUTH0_DOMAIN,
+  client_id: process.env.VUE_APP_AUTH0_CLIENT_ID,
   redirect_uri: window.location.origin,
-  audience: 'https://api.midni.ru',
+  audience: process.env.VUE_APP_AUTH0_AUDIENCE,
 });
 
 export default authProvider;
