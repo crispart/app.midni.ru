1e809475a2d3c169014aa649eae458347f704ad5
Roman Pashnev <crsprt@gmail.com>
2022-11-06 23:13:44 +0800

    Разрешить использовать до 120 символов в строке

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     'no-shadow': 'off',
     '@typescript-eslint/no-shadow': ['error'],
+    'max-len': ['error', { code: 120 }],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
