d288379977237d5c08b44d2cc7f954651ea9d772
Roman Pashnev <crsprt@gmail.com>
2022-11-07 00:36:32 +0800

    Перенести параметры конфигурации веб-клиента в .env

--- src/plugins/webClient.ts
+++ src/plugins/webClient.ts
@@ -1,28 +1,19 @@
 import { createFetch } from '@vueuse/core';
 import authProvider from '@/plugins/authProvider';
-import EnvironmentUtils from '@/utils/EnvironmentUtils';
-
-const requestTimeout: number = EnvironmentUtils.isProduction() ? 1000 * 10 : 1000 * 60;
 
 const webClient = createFetch({
-  baseUrl: 'https://api.midni.ru/api',
+  baseUrl: process.env.VUE_APP_RESOURCE_SERVER_URL,
   options: {
-    async beforeFetch({ options, cancel }) {
+    timeout: process.env.VUE_APP_HTTP_REQUEST_TIMEOUT,
+    async beforeFetch({ options }) {
       const accessToken = await authProvider.getAccessTokenSilently();
-
-      if (!accessToken) {
-        cancel();
-      }
-
       // eslint-disable-next-line no-param-reassign
       options.headers = {
         ...options.headers,
         Authorization: `Bearer ${accessToken}`,
       };
-
       return { options };
     },
-    timeout: requestTimeout,
   },
   fetchOptions: { mode: 'cors' },
 });
